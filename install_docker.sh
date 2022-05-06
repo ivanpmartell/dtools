18698de91d976259308d023fc49eadbf2d30a863
Ivan <ivan.pmartell@hotmail.com>
2022-05-05 22:32:03 -0700

    Fixed minor errors in docker installation script

--- install_docker.sh
+++ install_docker.sh
@@ -11,13 +11,13 @@ apt-get install -y \
     software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-apt install docker-ce
+apt install -y docker-ce
 apt-get autoremove --purge -y
 apt-get autoclean -y
 rm -rf /var/cache/apt/*
 
-echo "Fixing user permissions..."
-usermod -aG docker ${USER}
+echo "Fixing user permissions for $SUDO_USER..."
+usermod -aG docker $SUDO_USER
 echo "Done"
 echo "Make sure to relog into your user:"
-echo "\t\t su - ${USER}"
+echo "        su - $SUDO_USER"
